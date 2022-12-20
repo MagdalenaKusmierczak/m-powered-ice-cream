6826e890762fe66a94dd3d77604983af14687c1d
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2022-12-20 12:05:09 +0100

    Button unable

--- src/buy-now-modal.js
+++ src/buy-now-modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[buy-now-modal-open]'),
+    openModalBtn2: document.querySelector('[buy-now-modal-open2]'),
     closeModalBtn: document.querySelector('[buy-now-modal-close]'),
     modal: document.querySelector('[buy-now-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
